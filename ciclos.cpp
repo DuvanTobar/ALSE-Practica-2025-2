08cc640bd34f4f77f8718a5f92c5eb61e1c61e05
DuvanTobar <duvan.tobar-s@mail.escuelaing.edu.co>
2025-09-26 02:01:37 +0000

    Switch Y While.

---
+++ ciclos.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int opcion;
+    char r;
+    cout << "Desea ingresar alguna opcion?s/n:" << endl;
+    cin >> r;
+    while( r== 's' || r== 'S')
+    {
+        cout << "Por favor digite un numero entre 1 al 4 dependiendo de la opcion que desea: " << endl;
+        cin >> opcion;
+        switch (opcion)
+        {
+        case 1:
+            cout << "Usted ha seleccionado la opcion 1" << endl;
+            printf("HOLA MUNDO!\n");
+            break;
+        case 2:
+            cout << "Opcion 2 Seleccionada" << endl;
+            printf("FELICIDADES!\n");
+            break;
+        case 3:
+            cout << "Opcion 3 Seleccionada" << endl;
+            printf("USTED ES UN GENIO!\n");
+            break;
+
+        }
+   
+    }
+    return 0;
+
+}
